ae40a42ec4ea75e446a0e4c4f7d4fb0b6924edc5
NoeliaCI <>
2023-09-19 23:48:34 +0000

    Add variables and arrays

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+//Variables
+const letterContainer = document.getElementById("letter-container");
+const optionsContainer = document.getElementById("options-container");
+const userInputSection = document.getElementById("user-input-section");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const resultText = document.getElementById("result-text");
+
+//Arrays
+let options = {
+    animals: [
+        "Giraffe",
+        "Kangaroo",
+        "Cheetah",
+        "Monkey",
+        "Salamander",
+        "Orangutan",
+    ],
+    music: [
+        "Madonna",
+        "Metallica",
+        "Nirvana",
+        "Slipknot",
+        "Queen",
+        "Shakira",
+    ],
+    instruments: [
+        "Clarinet",
+        "Violoncello",
+        "Saxophone",
+        "Accordion",
+        "Harmonica",
+        "Xylophone",
+    ],
+};
