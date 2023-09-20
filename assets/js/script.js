8cfcf95cb40d900c20f6f7ab356429ec5879564b
NoeliaCI <>
2023-09-20 17:44:35 +0000

    Add variables for rules screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 //Variables
-const homeContainer = document.getElementsByClassName("home-container");
+const homeContainer = document.getElementById("home-container");
 const playButton = document.getElementById("play-button");
+const gameContainer = document.getElementById("game-container");
 const rulesButton = document.getElementById("rules-button");
+const homeButton = document.getElementById("home-button");
+const rulesContainer = document.getElementById("rules-container");
 const letterContainer = document.getElementById("letter-container");
 const optionsContainer = document.getElementById("options-container");
 const userInputSection = document.getElementById("user-input-section");
@@ -37,4 +40,36 @@ let options = {
         "Xylophone",
     ],
 };
+
+//Event Listeners
+
+/**
+ * Listens for the click of the play button in the home screen
+ * adds or removes the hide class
+ * enters the game screen
+ */
+
+playButton.addEventListener('click', () => {
+    homeContainer.classList.add("hide");
+    gameContainer.classList.remove("hide");
+});
+/**
+ * Listens for the click of the rules button in the home screen
+ * adds or removes the hide class
+ * enters the rules screen
+ */
+rulesButton.addEventListener('click', () => {
+    homeContainer.classList.add("hide");
+    rulesContainer.classList.remove("hide");
+});
+/**
+ * Listens for the click of the home button in the rules screen
+ * adds or removes the hide class
+ * goes back to the home screen
+ */
+homeButton.addEventListener('click', () => {
+    rulesContainer.classList.add("hide");
+    homeContainer.classList.remove("hide");
+});
+
 
