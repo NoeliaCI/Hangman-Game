64a6f76f42a6d52aca9da4cdfe318a391d094d13
NoeliaCI <>
2023-09-20 20:47:11 +0000

    Add code to create canvas for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ homeButton.addEventListener('click', () => {
     rulesContainer.classList.add("hide");
     homeContainer.classList.remove("hide");
 });
-
+//Functions from tutorial in https://www.youtube.com/watch?v=T6uht1A0114&list=PLNCevxogE3fgy0pAzVccadWKaQp9iHspz&index=24&ab_channel=CodingArtist
 // Variables for the count
 let winCount = 0;
 let count = 0;
@@ -80,7 +80,7 @@ let chosenWord = "";
 
 //Display the option buttons
 const displayOptions = () => {
-    optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
+    optionsContainer.innerHTML += `<h3>Please Select A Category</h3>`;
     let buttonCon = document.createElement("div");
     for (let value in options) {
         buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
@@ -187,5 +187,63 @@ const initializer = () => {
     //initialDrawing would draw the frame
     initialDrawing();
 };
+//Constant to create canvas
+const canvasCreator = () => {
+    let context = canvas.getContext("2d");
+    context.beginPath();
+    context.strokeStyle = "#000";
+    context.lineWidth = 2;
+    //For drawing lines
+    const drawLine = (fromX, fromY, toX, toY) => {
+        context.moveTo(fromX, fromY);
+        context.lineTo(toX, toY);
+        context.stroke();
+    };
+    const head = () => {
+        context.beginPath();
+        context.arc(70, 30, 10, 0, Math.PI * 2, true);
+        context.stroke();
+    };
+
+    const body = () => {
+        drawLine(70, 40, 70, 80);
+    };
+
+    const leftArm = () => {
+        drawLine(70, 50, 50, 70);
+    };
+
+    const rightArm = () => {
+        drawLine(70, 50, 90, 70);
+    };
+
+    const leftLeg = () => {
+        drawLine(70, 80, 50, 110);
+    };
+
+    const rightLeg = () => {
+        drawLine(70, 80, 90, 110);
+    };
+    //Initial frame
+    const initialDrawing = () => {
+        //Clear the canvas
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        //Bottom line
+        drawLine(10, 130, 130, 130);
+        //Left line
+        drawLine(10, 10, 10, 131);
+        //Top line
+        drawLine(10, 10, 70, 10);
+        //Small top line
+        drawLine(70, 10, 70, 20);
+    };
+    return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
+};
+
+//New Game
+newGameButton.addEventListener("click", initializer);
+window.onload = initializer;
+
+
 
 
