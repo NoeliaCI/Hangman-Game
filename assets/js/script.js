497f1a8f5fd640aa4fb8ec5b99ca356767248828
NoeliaCI <>
2023-09-21 12:40:10 +0000

    Add code to draw the man

--- assets/js/script.js
+++ assets/js/script.js
@@ -239,6 +239,32 @@ const canvasCreator = () => {
     };
     return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
 };
+//Code to draw the man
+const drawMan = (count) => {
+    let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
+    switch (count) {
+        case 1:
+            head();
+            break;
+        case 2:
+            body();
+            break;
+        case 3:
+            leftArm();
+            break;
+        case 4:
+            rightArm();
+            break;
+        case 5:
+            leftLeg();
+            break;
+        case 6:
+            rightLeg();
+            break;
+        default:
+            break;
+    }
+};
 
 //New Game
 newGameButton.addEventListener("click", initializer);
